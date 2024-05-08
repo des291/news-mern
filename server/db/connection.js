64b41437ba7de3ae4ae5bd57b24836d83fa0a899
Des <des.grainger@proton.me>
2024-05-08 22:41:19 +0100

    set up db

---
+++ server/db/connection.js
@@ -0,0 +1,23 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+import "dotenv/config";
+
+const uri = process.env.ATLAS_URI || "";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+try {
+  await client.connect();
+  await client.db("admin").command({ ping: 1 });
+  console.log("Pinged your deployment. You successfully connected to MongoDB!");
+} catch (err) {
+  console.error(err);
+}
+
+let db = client.db("articles-collection");
+
+export default db;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 import { express } from "express";
 import { cors } from "cors";
 import { index } from "./routes/index.js";
+import "dotenv/config";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/", index);
+
+// start the Express server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
